014986bb8950b69e5fb409285e2547efb7543313
KolosovAA <andre.e.04@gmail.com>
2023-04-07 22:45:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+import java.util.function.Predicate;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+    }
+}
+
+class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? x : x / y; //пришлось изменить код так как нельзя делить на 0
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    UnaryOperator<Integer> abss = x -> {
+        if (x > 0) return x;
+        else return x * (-1);
+    }; //для тренировки сделал еще один вариант записи abs
+
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
